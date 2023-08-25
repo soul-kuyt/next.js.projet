4984af0ab15e24741d8bf1f59a9af875ddb51a80
soul-kuyt <120533066+soul-kuyt@users.noreply.github.com>
2023-08-25 15:26:08 +0000

    Changes to be committed: 	new file:   my-app/.eslintrc.json 	new file:   my-app/.gitignore 	new file:   my-app/README.md 	new file:   my-app/app/about/page.js 	new file:   my-app/app/favicon.ico 	new file:   my-app/app/globals.css 	new file:   my-app/app/layout.js 	new file:   my-app/app/page.js 	new file:   my-app/components/about/About.js 	new file:   my-app/jsconfig.json 	new file:   my-app/next.config.js 	new file:   my-app/package-lock.json 	new file:   my-app/package.json 	new file:   my-app/postcss.config.js 	new file:   my-app/public/next.svg 	new file:   my-app/public/vercel.svg 	new file:   my-app/tailwind.config.js

---
+++ my-app/app/about/page.js
@@ -0,0 +1,11 @@
+import About from "@/components/about/About"
+
+export default function Apropos(){
+    return(
+        <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className='text-3xl'> About </h1>
+      
+      <About/>
+    </main>
+    )   
+}
---
+++ my-app/app/page.js
@@ -0,0 +1,10 @@
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className='text-3xl'> Acceuil </h1>
+      
+    </main>
+  )
+}
